dcb6b4ce16541be9fe41964d9481cdc136980cf4
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-07-14 12:31:45 +0300

    init

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  platform: "github",
+  token: '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',
+  onboarding: true,
+  requireConfig: false,
+  includeForks: true,
+  repositories: [
+    "ladzaretti/package-lock-downgrade-testing",
+  ],
+  "dependencyDashboard": true,
+  logFile: "/tmp/renovate.log",
+  logFileLevel: "debug",
+};
